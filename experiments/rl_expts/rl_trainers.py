64e79af867e45d02e3baedcb94bf7948b2d3146c
ujan <ujandev@gmail.com>
2024-11-03 18:02:03 +0530

    ppo test

--- experiments/rl_expts/rl_trainers.py
+++ experiments/rl_expts/rl_trainers.py
@@ -617,16 +617,7 @@ class PPOTrainer(RLTrainer):
         
         ## housekeeping ##
         stats = self.stack_dict_batches(stats)
-        print(stats['policy/advantages'])
-        # reshape advantages/ratios such that they are not averaged.
-        try:
-            stats['policy/advantages'] = torch.flatten(stats['policy/advantages']).unsqueeze(0)
-            stats['policy/ratio'] = torch.flatten(stats['policy/ratio']).unsqueeze(0)
-        except TypeError as e:
-            train_stats['policy/advantages'] = None
-            train_stats['policy/ratio'] = None
-            print('')
-        print(stats['policy/advantages'])
+        print(stats['objective/kl'].shape)
         quit()
 
         self.kl_ctl.update(stats['objective/kl'], self.args.batch_size)
